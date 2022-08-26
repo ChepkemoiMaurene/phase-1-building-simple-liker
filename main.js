67e547e2dbd0807d4b0f10682558b22ce7ce99a9
ChepkemoiMaurene <baurene.chepkemoi@student.moringaschool.com>
2022-08-26 12:02:12 +0300

    completed assignments

--- main.js
+++ main.js
@@ -1,15 +1,39 @@
-// Defining text characters for the empty and full hearts for you to use later.
+
 const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
-// Your JavaScript code goes here!
 
 
+//CODE BEGINS HERE 
+const errorModel = document.getElementById('modal')
+errorModel.classList.add('hidden') 
 
 
-//------------------------------------------------------------------------------
-// Don't change the code below: this function mocks the server response
-//------------------------------------------------------------------------------
+const heartClicked = (event) => { 
+  
+  mimicServerCall()  
+  .then(()=>{  
+    if(event.target.textContent == EMPTY_HEART){  
+      event.target.textContent = FULL_HEART       
+      event.target.classList.add('activated-heart') 
+    }else{
+      event.target.textContent = EMPTY_HEART     
+      event.target.classList.remove('activated-heart')   
+    }
+    
+  })
+  .catch(()=>{  
+    errorModel.classList.remove('hidden')
+    setTimeout(function(){  
+      errorModel.classList.add('hidden') 
+    }, 3000)
+  })
+  
+}
+
+let hearts = document.getElementsByClassName('like-glyph') 
+for (let heart of hearts) { 
+  heart.addEventListener('click', heartClicked) 
 
 function mimicServerCall(url="http://mimicServer.example.com", config={}) {
   return new Promise(function(resolve, reject) {
@@ -22,4 +46,4 @@ function mimicServerCall(url="http://mimicServer.example.com", config={}) {
       }
     }, 300);
   });
-}
+}}
